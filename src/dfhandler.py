c0b482aa46b3196e3674c0d515d4f97293f252d3
Dokujaa <Srikar.krothapalli@gmail.com>
2024-10-16 15:45:33 -0400

    Added randomization to the twitter bot

--- src/dfhandler.py
+++ src/dfhandler.py
@@ -1,10 +1,9 @@
+import datetime
 import os
 import pandas as pd
-import datetime
-
+import random
 
 def create_dataframe_from_csv(csv_directory):
-  
     today_date_str = datetime.datetime.now().strftime("%Y-%m-%d")
 
     csv_files = [f for f in os.listdir(csv_directory) if f.endswith('.csv')]
@@ -18,25 +17,28 @@ def create_dataframe_from_csv(csv_directory):
     # Use the first matching file
     csv_file_path = os.path.join(csv_directory, matching_files[0])
 
-
     return pd.read_csv(csv_file_path)
 
-
 def generate_tweet_message(df):
-    """Parses the DataFrame to generate the tweet message for the top-ranked track."""
-    # Get the top-ranked track (Rank 1)
-    top_track = df[df['Rank'] == 1].iloc[0]
+    """Parses the DataFrame to generate a random tweet message with specific rank for a track."""
+    # Randomly select a row from the DataFrame
+    random_track = df.sample(n=1).iloc[0]
+
+    # Get the specific rank
+    rank = random_track['Rank']
 
     # Format the artist names
-    artist_names = top_track['Artists'].split(", ")
+    artist_names = random_track['Artists'].split(", ")
     artist_names_text = " and ".join(artist_names) if len(artist_names) > 1 else artist_names[0]
 
     # Extract the track name and popularity
-    track_name = top_track['Track Name']
-    popularity = top_track['Popularity']
+    track_name = random_track['Track Name']
+    popularity = random_track['Popularity']
 
     # Get today's date in "Month DD, YYYY" format for the tweet
     today_date = datetime.datetime.now().strftime("%B %d, %Y")
 
     # Create and return the tweet message
-    return f"\"{track_name}\" by {artist_names_text} is currently rank 1 on Spotify on {today_date}, with a popularity score of {popularity}! TESTSTS"
+    return f"\"{track_name}\" by {artist_names_text} is currently ranked #{rank} on Spotify on {today_date}, with a popularity score of {popularity}!"
+
+
